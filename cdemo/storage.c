1e4b20fd95902b807609e393d543005aa5859c56
student1kc <gregstanleyidris@gmail.com>
2018-05-25 16:12:57 +0000

    your commit

---
+++ cdemo/storage.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <limits.h>
+
+int main() {
+	printf("Storage size for int : %d\n", sizeof(int));
+	printf("Storage size for float : %d\n", sizeof(float));
+	printf("Storage size for double: %d\n", sizeof(double));
+	printf("Storage size of char: %d\n", sizeof(char));
+	return 0;
+}
