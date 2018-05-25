d6cceb959531e9a475fffbf5f73e1fbd5c940673
student1kc <gregstanleyidris@gmail.com>
2018-05-25 20:14:04 +0000

    your commit

---
+++ cdemo/areaofCircle.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+float areaOfCircle(float i)
+{
+	float x;
+	x = i * i* 3.14;
+	printf("%f\n", x);
+	return x;
+}
+
+int main ()
+{
+	float i;
+	for (i = 3.5; i < 12.5; i++)
+	{
+		areaOfCircle(i);
+	}
+}
