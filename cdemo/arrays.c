76f7002754c4d0998256dba6a79755e43b7af78e
student1kc <38955619+student1kc@users.noreply.github.com>
2018-05-28 15:12:24 -0700

    Add files via upload

---
+++ cdemo/arrays.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int arrayAdd(int* arr, int s, int n)
+{
+	for (int i =0; i < s; i++)
+	{
+		arr[i] = i * i;
+		arr[i] = arr[i] + n;
+		printf ("%d\n",arr[i]);
+	}
+}
+
+int main()
+{
+	int arr[100];
+	for (int i = 0; i < 100; i++)
+	{
+		arr[i] = i * i;
+	}
+
+	int s = 100;
+	int n = 3;
+	arrayAdd( arr, s, n);
+}
